4c80e1ffac8e56a49eb70cb1df40aa81ecfdd650
Kyle Middleton <kmiddleton654@gmail.com>
2018-06-27 13:04:34 -0600

    even more troubleshooting

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://obscure-cliffs-37897.herokuapp.com',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://localhost/react-tictactoe-capstone',
   TEST_DATABASE_URL:

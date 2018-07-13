7e352ee6d12b187489014ec1fd72a210561cd997
Kyle Middleton <kmiddleton654@gmail.com>
2018-07-13 16:51:41 -0600

    server changes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://obscure-cliffs-37897.herokuapp.com' || 'https://kmid5280.github.io/capstone/',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://obscure-cliffs-37897.herokuapp.com',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://localhost/react-tictactoe-capstone',
   TEST_DATABASE_URL:

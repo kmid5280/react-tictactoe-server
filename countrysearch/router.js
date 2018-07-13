1d663b554dd61d70518fdf835a0401594e5ac5db
Kyle Middleton <kmiddleton654@gmail.com>
2018-07-13 12:06:44 -0600

    added module exports in router

--- countrysearch/router.js
+++ countrysearch/router.js
@@ -11,3 +11,5 @@ router.get('/', (req, response) => {
     .then(json => response.status(200).json(json))
     .catch(err => res.status(500).json({message: 'Internal server error'}))
 });
+
+module.exports = {router};
